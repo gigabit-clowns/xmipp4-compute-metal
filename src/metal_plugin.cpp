f810809cd95f984597d1f330172621e0ab286288
Ratolon <miceta@cnb.csic.es>
2025-09-04 22:45:06 +0200

    Change registry for catalog

--- src/metal_plugin.cpp
+++ src/metal_plugin.cpp
@@ -2,9 +2,9 @@
 
 #include "metal_plugin.hpp"
 
-#include "compute/metal/metal_device_backend.hpp"
+#include "xmipp4/metal/compute/metal_device_backend.hpp"
 
-#include <xmipp4/core/interface_registry.hpp>
+#include <xmipp4/core/interface_catalog.hpp>
 #include <xmipp4/core/compute/device_manager.hpp>
 
 namespace xmipp4 
@@ -26,10 +26,10 @@ version metal_plugin::get_version() const noexcept
     );
 }
 
-void metal_plugin::register_at(interface_registry& registry) const
+void metal_plugin::register_at(interface_catalog& catalog) const
 {
     compute::metal_device_backend::register_at(
-        registry.get_interface_manager<compute::device_manager>()
+        catalog.get_interface_manager<compute::device_manager>()
     );
 }
 
