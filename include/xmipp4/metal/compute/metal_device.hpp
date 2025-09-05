69429f0f57cea1deb6ebbc24b0fa5e57edd973fb
Ratolon <miceta@cnb.csic.es>
2025-09-05 21:13:15 +0200

    initial metal_device

---
+++ include/xmipp4/metal/compute/metal_device.hpp
@@ -0,0 +1,61 @@
+// SPDX-License-Identifier: GPL-3.0-only
+
+#pragma once
+
+#include <Metal/Metal.hpp>
+#include <xmipp4/core/compute/device.hpp>
+
+#include "metal_device_queue_pool.hpp"
+
+namespace xmipp4
+{
+namespace compute
+{
+
+class device_create_parameters;
+
+class metal_device final
+    : public device
+{
+  public:
+    metal_device(MTL::Device id, const device_create_parameters &params);
+    metal_device(const metal_device &other) = delete;
+    metal_device(metal_device &&other) = default;
+    ~metal_device() override = default;
+
+    metal_device& operator=(const metal_device &other) = delete;
+    metal_device& operator=(metal_device &&other) = default;
+
+    int get_index() const noexcept;
+
+    //metal_device_queue_pool& get_queue_pool() override;
+
+    std::shared_ptr<device_memory_allocator>
+    create_device_memory_allocator() override;
+
+    std::shared_ptr<host_memory_allocator>
+    create_host_memory_allocator() override;
+
+    std::shared_ptr<host_to_device_transfer>
+    create_host_to_device_transfer() override;
+
+    std::shared_ptr<device_to_host_transfer>
+    create_device_to_host_transfer() override;
+
+    std::shared_ptr<device_copy>
+    create_device_copy() override;
+
+    std::shared_ptr<device_event>
+    create_device_event() override;
+
+    std::shared_ptr<device_to_host_event>
+    create_device_to_host_event() override;
+
+    private:
+        MTL::Device m_device;
+        //metal_device_queue_pool m_queue_pool;
+
+};
+
+} // namespace compute
+} // namespace xmipp4
