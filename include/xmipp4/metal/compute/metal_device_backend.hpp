d47cf1154999a04f4474223fae1171ce755ec788
Ratolon <miceta@cnb.csic.es>
2025-09-06 17:13:44 +0200

    update interface

--- include/xmipp4/metal/compute/metal_device_backend.hpp
+++ include/xmipp4/metal/compute/metal_device_backend.hpp
@@ -3,6 +3,7 @@
 #pragma once
 
 #include <xmipp4/core/compute/device_backend.hpp>
+#include <xmipp4/core/compute/device_create_parameters.hpp>
 
 namespace xmipp4 
 {
@@ -11,22 +12,21 @@ namespace compute
 
 class device_manager;
 
-
-
 class metal_device_backend final
     : public device_backend
 {
 public:
-    std::string get_name() const noexcept final;
-    version get_version() const noexcept final;
-    bool is_available() const noexcept final;
-    backend_priority get_priority() const noexcept final;
+    std::string get_name() const noexcept override;
+    version get_version() const noexcept override;
+    bool is_available() const noexcept override;
+    backend_priority get_priority() const noexcept override;
 
-    void enumerate_devices(std::vector<std::size_t> &ids) const final;
-    bool get_device_properties(std::size_t id, device_properties &desc) const final;
+    void enumerate_devices(std::vector<std::size_t> &ids) const override;
+    bool get_device_properties(std::size_t id, device_properties &desc) const override;
 
-    std::unique_ptr<device> create_device(std::size_t id, const device_create_parameters &params) final;
-    std::shared_ptr<device> create_device_shared(std::size_t id, const device_create_parameters &params) final;
+    std::shared_ptr<device> 
+    create_device(std::size_t id, 
+                    const device_create_parameters &params) override;
 
     static bool register_at(device_manager &manager);
 
